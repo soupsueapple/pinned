43f282c66996e094146dad2b5012992abf835d8b
soupsueapple <soupsueapple@gmail.com>
2017-03-30 16:46:27 +0800

    Cocoapods

---
+++ Podfile
@@ -0,0 +1,9 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '8.0'
+use_frameworks!
+
+target 'pinned' do
+pod 'AFNetworking', '~> 3.0'
+
+pod 'SwiftyJSON'
+end
---
+++ pinned/OCHeader.h
@@ -0,0 +1,15 @@
+//
+//  OCHeader.h
+//  pinned
+//
+//  Created by 汤坤 on 2017/3/30.
+//  Copyright © 2017年 Kun Soup. All rights reserved.
+//
+
+#import "OnePasswordExtension.h"
+
+#ifndef OCHeader_h
+#define OCHeader_h
+
+
+#endif /* OCHeader_h */
